dd15f957d21639a22da711de3e902837b6153639
Kris <kri350@gmail.com>
2021-10-23 20:24:18 +0300

    value changer

--- HW.2.2/ViewController.swift
+++ HW.2.2/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var tintView: UIView!
     
+    @IBOutlet var redValue: UILabel!
+    @IBOutlet var greenValue: UILabel!
+    @IBOutlet var blueValue: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tintView.layer.cornerRadius = 15
     }
 
-
+    @IBAction func redSliderAction() {
+        redValue.text = String(format: "%.3f", redSlider.value)
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenValue.text = String(format: "%.3f", greenSlider.value)
+    }
+    @IBAction func blueSliderAction() {
+        blueValue.text = String(format: "%.3f", blueSlider.value)
+        
+    }
 }
 
