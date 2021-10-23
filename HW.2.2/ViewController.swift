d18ba8a204ba1618d4e3147c4ae5b929ab6e718a
Kris <kri350@gmail.com>
2021-10-23 16:08:56 +0300

    Initial Commit

---
+++ HW.2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW.2.2
+//
+//  Created by Кристина Андреева on 23.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
